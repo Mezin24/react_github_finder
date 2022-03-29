9026f63640f7dbeca9a74b9371555ef318466a8d
Pavel Mezencev <mezencev24@mail.ru>
2022-03-29 23:12:42 +0300

    add spiner

---
+++ src/components/assets/Spinner.jsx
@@ -0,0 +1,11 @@
+import { FaSpinner } from 'react-icons/fa'
+
+function Spinner() {
+  return (
+    <div>
+      <FaSpinner className="animate-spin mx-auto" size="80" />
+    </div>
+  )
+}
+
+export default Spinner
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import UserResults from '../components/users/UserResults'
 
 function Home() {
   return (
-    <div>
-      <h1 className="text-6xl mb-4">Welcome</h1>
-      {process.env.REACT_APP_GITHUB_TOKEN}
-    </div>
+    <>
+      <UserResults />
+    </>
   )
 }
 
